838c4cdd76e73241876d044334a309cd816b4671
Zuzuitu <178236894+Zuzuitu@users.noreply.github.com>
2026-09-13 22:53:31 +0200

    Prepare Flud Companion 0.24.9

--- app/src/main/java/media/alexlab/fludremote/BridgeHttpServer.kt
+++ app/src/main/java/media/alexlab/fludremote/BridgeHttpServer.kt
@@ -21,7 +21,7 @@ class BridgeHttpServer(
     companion object {
         private const val MAX_HEADER_BYTES = 16 * 1024
         private const val MAX_BODY_BYTES = 16 * 1024
-        const val VERSION = "0.24.9-rc1"
+        const val VERSION = "0.24.9"
     }
 
     private val running = AtomicBoolean(false)
